16ee757014921c20de441429b3d27630001310de
Kamil Sikora <kamilsikora@Kamil-Sikora-Kellton-MacBook-Pro-YW9DX0N7L4.local>
2023-04-16 22:22:06 +0200

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useEffect} from "react";
+import firestore from '@react-native-firebase/firestore';
 
 export default function App() {
+
+
+  useEffect(() => {
+
+    (async () => {
+      const users = await firestore().collection('Users').doc('Fdl1XP0hb8fZL3RzFjXk').get();
+
+      console.log(users)
+    })()
+  })
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
