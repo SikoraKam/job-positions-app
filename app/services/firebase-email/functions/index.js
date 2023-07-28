59911f71fc79e4908ac2e1a7540e2dc938652ce8
Kamil Sikora <kamilsikora@Kamil-Sikora-Kellton-MacBook-Pro-YW9DX0N7L4.local>
2023-07-28 23:51:29 +0200

    added email sending when offer accepted

--- app/services/firebase-email/functions/index.js
+++ app/services/firebase-email/functions/index.js
@@ -45,35 +45,3 @@ exports.submit = functions.https.onRequest((req, res) => {
     });
   }
 });
-
-// exports.mailTo = functions.https.onCall((req, res) => {
-//   res.set("Access-Control-Allow-Origin", "*");
-//   res.set("Access-Control-Allow-Methods", "GET, PUT, POST, OPTIONS");
-//   res.set("Access-Control-Allow-Headers", "*");
-//
-//   if (req.method === "OPTIONS") {
-//     res.end();
-//   } else {
-//     cors(req, res, () => {
-//       if (req.method !== "POST") {
-//         return;
-//       }
-//
-//       const mailOptions = {
-//         from: gmailEmail,
-//         to: req.body.email,
-//         subject: `Job positions app - new application from ${req.body.name}`,
-//         text: req.body.message,
-//         html: `<p>${req.body.message}</p>`,
-//       };
-//
-//       return mailTransport.sendMail(mailOptions).then(() => {
-//         console.log("New email sent to:", gmailEmail);
-//         res.status(200).send({
-//           isEmailSend: true,
-//         });
-//         return;
-//       });
-//     });
-//   }
-// });
